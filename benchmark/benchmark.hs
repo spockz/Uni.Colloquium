1efb8eb1a3fc40cc3ce1369aee08c304d45c17b2
Alessandro Vermeulen <github@spockz.nl>
2012-02-23 14:28:12 +0100

    Added UHC benchmark

---
+++ benchmark/benchmark.hs
@@ -0,0 +1,15 @@
+module Main where
+  
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = let l = fib (n-1)
+            r = fib (n-2)
+            res = l + r
+         in res
+
+main = do f 100
+ where x   = 30
+       f 1 = print $ fib x
+       f n = do print $ fib x
+                f (n-1)
